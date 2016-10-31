e7a86b6a98c2d7c4c2c2fee96a3fa4bb1b680e0e
无名 <952532573@qq.com>
2016-10-31 09:05:51 +0800

    update

---
+++ tests/com/wuming/junit/DailyTest.java
@@ -0,0 +1,17 @@
+package com.wuming.junit;
+
+import org.junit.Test;
+
+/**
+ * Created by wuming on 16/10/30.
+ */
+public class DailyTest {
+
+    @Test
+    public void test(){
+        Object object = new Integer(100);
+        String a = (String) object;
+
+
+    }
+}
