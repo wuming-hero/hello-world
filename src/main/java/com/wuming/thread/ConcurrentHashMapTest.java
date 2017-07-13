37c57356eeb65ef2d2cd1636ab4c8ff21f9339b4
wuming <952532573@qq.com>
2017-07-13 19:39:39 +0800

    update ConcurrentHashMap demo

---
+++ src/main/java/com/wuming/thread/ConcurrentHashMapTest.java
@@ -0,0 +1,243 @@
+package com.wuming.thread;
+
+import com.google.common.base.MoreObjects;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * Created by wuming on 2017/7/13.
+ * 1、public V get(Object key)不涉及到锁，也就是说获得对象时没有使用锁；
+ * <p>
+ * 2、put、remove方法要使用锁，但并不一定有锁争用，原因在于ConcurrentHashMap将缓存的变量分到多个Segment，
+ * 每个Segment上有一个锁，只要多个线程访问的不是一个Segment就没有锁争用，就没有堵塞，各线程用各自的锁，
+ * ConcurrentHashMap缺省情况下生成16个Segment，也就是允许16个线程并发的更新而尽量没有锁争用；
+ * <p>
+ * 3、Iterator对象的使用，不一定是和其它更新线程同步，获得的对象可能是更新前的对象，
+ * ConcurrentHashMap允许一边更新、一边遍历，也就是说在Iterator对象遍历的时候，
+ * ConcurrentHashMap也可以进行remove,put操作，且遍历的数据会随着remove,put操作产出变化，
+ * 所以希望遍历到当前全部数据的话，要么以ConcurrentHashMap变量为锁进行同步(synchronized该变量)，
+ * 要么使用CopiedIterator包装iterator，使其拷贝当前集合的全部数据，但是这样生成的iterator不可以进行remove操作。
+ * <p>
+ * 一个线程对ConcurrentHashMap增加数据，另外一个线程在遍历时就能获得
+ */
+public class ConcurrentHashMapTest {
+
+    static Map<Long, String> conMap = new ConcurrentHashMap<>();
+
+//    public static void main(String[] args) throws InterruptedException {
+//        for (long i = 0; i < 5; i++) {
+//            conMap.put(i, i + "");
+//        }
+//
+//        Thread thread = new Thread(() -> {
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            conMap.put(100l, "100");
+//            System.out.println("ADD:" + 100);
+//        });
+//
+//        Thread thread2 = new Thread(() -> {
+//            for (Iterator<Map.Entry<Long, String>> iterator = conMap.entrySet().iterator(); iterator.hasNext(); ) {
+//                Map.Entry<Long, String> entry = iterator.next();
+//                System.out.println(entry.getKey() + " - " + entry.getValue());
+//                try {
+//                    Thread.sleep(100);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//        thread.start();
+//        thread2.start();
+//
+//        Thread.sleep(3000);
+//        System.out.println("--------");
+//
+//        for (Map.Entry<Long, String> entry : conMap.entrySet()) {
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        }
+//
+//    }
+
+    /**
+     * 边遍历边做删除操作，HashMap 或ArrayList 会报java.util.ConcurrentModificationException异常
+     * CConcurrentHashMap 不使用iterator，直接遍历删除则不会报错
+     */
+    @Test
+    public void hashMapTest() {
+        Map<Long, String> mReqPacket = new HashMap<>();
+        for (long i = 0; i < 15; i++) {
+            mReqPacket.put(i, i + "");
+        }
+
+        // 边遍历边删除操作，会报java.util.ConcurrentModificationException异常
+        for (Map.Entry<Long, String> entry : mReqPacket.entrySet()) {
+            long key = entry.getKey();
+            String value = entry.getValue();
+            if (key < 10) {
+                mReqPacket.remove(key);
+            }
+        }
+
+        // 使用 Iterator 迭代器则可正常操作
+        for (Iterator<Map.Entry<Long, String>> iterator = mReqPacket.entrySet().iterator(); iterator.hasNext(); ) {
+            Map.Entry<Long, String> entry = iterator.next();
+            long key = entry.getKey();
+            if (key < 10) {
+                iterator.remove();
+            }
+        }
+
+        for (Map.Entry<Long, String> entry : mReqPacket.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+    @Test
+    public void hashMapTest2() {
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        CountDownLatch endLatch = new CountDownLatch(2);
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                if (map.containsKey("a")) {
+                    map.put("a", map.get("a") + 1);
+                } else {
+                    map.put("a", 1);
+                }
+//                Integer value = map.get("a");
+//                if (null == value) {
+//                    map.put("a", 1);
+//                } else {
+//                    map.put("a", value + 1);
+//                }
+            }
+            endLatch.countDown();
+        };
+
+        new Thread(task).start();
+        new Thread(task).start();
+
+        try {
+            endLatch.await();
+            System.out.println(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        for (int i = 0; i < 12; i++) {
+            executorService.execute(() -> {
+                if (map.containsKey("a")) {
+                    map.put("a", map.get("a") + 1);
+                } else {
+                    map.put("a", 1);
+                }
+                try {
+                    Thread.sleep(5 * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executorService.shutdown();
+
+        Long time = System.currentTimeMillis();
+        System.out.println("before: " + time);
+
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+
+//        while (!executorService.awaitTermination(1, TimeUnit.SECONDS)) {
+//            System.out.println("sleep----");
+//            Thread.sleep(1000);
+//        }
+        System.out.println(System.currentTimeMillis() - time);
+        System.out.println(map);
+    }
+
+//    @Test
+//    public void test2() throws InterruptedException {
+//        ExecutorService executorService = Executors.newFixedThreadPool(20);
+//        Map<String, Integer> concurrentMap = new ConcurrentHashMap<>();
+//        List<Map<String, Integer>> mapList = new ArrayList<>();
+//
+//        Map<String, Integer> dataMap;
+//        for (int i = 0; i < 1000; i++) {
+//            dataMap = new HashMap<>();
+//            dataMap.put("a", 1);
+//            mapList.add(dataMap);
+//        }
+//
+//        for (Map<String, Integer> map: mapList) {
+//            executorService.execute(() -> {
+////                if (concurrentMap.containsKey("a")) {
+////                    concurrentMap.put("a", concurrentMap.get("a") + map.get("a"));
+////                } else {
+////                    concurrentMap.put("a", 1);
+////                }
+//                concurrentMap.put("a", MoreObjects.firstNonNull(concurrentMap.get("a"), 1) + map.get("a"));
+//            });
+//        }
+//        executorService.shutdown();
+//        executorService.awaitTermination(10, TimeUnit.SECONDS);
+//
+//        System.out.println(concurrentMap);
+//    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        Map<String, Integer> concurrentMap = new ConcurrentHashMap<>();
+        List<Map<String, Integer>> mapList = new ArrayList<>();
+
+        // 模拟数据
+        Map<String, Integer> dataMap;
+        for (int i = 0; i < 1000; i++) {
+            dataMap = new HashMap<>();
+            dataMap.put("a", 1);
+            mapList.add(dataMap);
+        }
+
+        for (Map<String, Integer> map: mapList) {
+            executorService.execute(() -> {
+                concurrentMap.put("a", MoreObjects.firstNonNull(concurrentMap.get("a"), 1) + map.get("a"));
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
+
+        System.out.println(concurrentMap);
+    }
+
+    /**
+     * 对ConcurrentHashMap边遍历边删除或者增加操作不会产生异常(可以不用迭代方式删除元素)，
+     * 因为其内部已经做了维护，遍历的时候都能获得最新的值。即便是多个线程一起删除、添加元素也没问题
+     */
+    @Test
+    public void concurrentHashMapTest() {
+        Map<Long, String> conMap = new ConcurrentHashMap<>();
+        for (long i = 0; i < 15; i++) {
+            conMap.put(i, i + "");
+        }
+
+        for (Map.Entry<Long, String> entry : conMap.entrySet()) {
+            long key = entry.getKey();
+            if (key < 10) {
+                conMap.remove(key);
+            }
+        }
+
+        for (Map.Entry<Long, String> entry : conMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+
+
+}
--- src/main/java/com/wuming/thread/MapTest.java
+++ src/main/java/com/wuming/thread/MapTest.java
@@ -1,17 +1,20 @@
 package com.wuming.thread;
 
+import org.junit.Test;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by wuming on 2017/7/1.
- * 线程安全map的性能测试
+ * <p>
+ * HashMap线程不安全：
+ * HashMap在并发执行put操作时会引起死循环，导致CPU利用率接近100%。
+ * 因为多线程会导致HashMap的Node链表形成环形数据结构，一旦形成环形数据结构，Node的next节点永远不为空，就会在获取Node时产生死循环。
  */
 public class MapTest {
 
@@ -20,6 +23,12 @@ public class MapTest {
     public static Map<String, Integer> synchronizedMap = null;
     public static Map<String, Integer> concurrentHashMap = null;
 
+    /**
+     * ConcurrentHashMap性能是明显优于 Hashtable 和 SynchronizedMap 的, ConcurrentHashMap 花费的时间比前两个的一半还少
+     *
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         // Test with Hashtable Object
         hashTable = new Hashtable<>();
@@ -41,17 +50,13 @@ public class MapTest {
             long startTime = System.nanoTime();
             ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
             for (int j = 0; j < THREAD_POOL_SIZE; j++) {
-                executorService.execute(new Runnable() {
-                    @SuppressWarnings("unused")
-                    @Override
-                    public void run() {
-                        for (int i = 0; i < 500000; i++) {
-                            Integer randomNumber = (int) Math.ceil(Math.random() * 550000);
-                            // Retrieve value. We are not using it anywhere
-                            Integer value = threadMap.get(String.valueOf(randomNumber));
-                            // Put value
-                            threadMap.put(String.valueOf(randomNumber), randomNumber);
-                        }
+                executorService.execute(() -> {
+                    for (int i1 = 0; i1 < 500000; i1++) {
+                        Integer randomNumber = (int) Math.ceil(Math.random() * 550000);
+                        // Retrieve value. We are not using it anywhere
+                        Integer value = threadMap.get(String.valueOf(randomNumber));
+                        // Put value
+                        threadMap.put(String.valueOf(randomNumber), randomNumber);
                     }
                 });
             }
@@ -67,4 +72,126 @@ public class MapTest {
         }
         System.out.println("For " + threadMap.getClass() + " the average time is " + averageTime / 5 + " ms\n");
     }
+
+
+    /********************demo start*************************/
+    /**
+     * ConcurrentHashMap是并发效率更高的Map，用来替换其他线程安全的Map容器，比如Hashtable和Collections.synchronizedMap。
+     * <p>
+     * 实际上，并发执行时，线程安全的容器只能保证自身的数据不被破坏，但无法保证业务的行为是否正确。
+     * <p>
+     * 错误的理解这里的线程安全，不恰当的使用ConcurrentHashMap，往往会导致出现问题。
+     * <p>
+     * demo1是两个线程操作ConcurrentHashMap，意图将value变为10。
+     * 但是，因为多个线程用相同的key调用时，很可能会覆盖相互的结果，造成记录的次数比实际出现的次数少
+     */
+    @Test
+    public void demo1() {
+        final Map<String, Integer> count = new ConcurrentHashMap<>();
+        final CountDownLatch endLatch = new CountDownLatch(2);
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                Integer value = count.get("a");
+                if (null == value) {
+                    count.put("a", 1);
+                } else {
+                    count.put("a", value + 1);
+                }
+            }
+            endLatch.countDown();
+        };
+        new Thread(task).start();
+        new Thread(task).start();
+
+        try {
+            endLatch.await();
+            System.out.println(count);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 使用ConcurrentMap定义的方法对demo1进行改进
+     * <p>
+     * V putIfAbsent(K key, V value)
+     * 如果key对应的value不存在，则put进去，返回null。否则不put，返回已存在的value。
+     * <p>
+     * boolean remove(Object key, Object value)
+     * 如果key对应的值是value，则移除K-V，返回true。否则不移除，返回false。
+     * <p>
+     * boolean replace(K key, V oldValue, V newValue)
+     * 如果key对应的当前值是oldValue，则替换为newValue，返回true。否则不替换，返回false。
+     */
+    @Test
+    public void demo2() {
+        final Map<String, Integer> count = new ConcurrentHashMap<>();
+        final CountDownLatch endLatch = new CountDownLatch(2);
+        Runnable task = () -> {
+            Integer oldValue, newValue;
+            for (int i = 0; i < 1000; i++) {
+                while (true) {
+                    oldValue = count.get("a");
+                    if (null == oldValue) {
+                        newValue = 1;
+                        if (count.putIfAbsent("a", newValue) == null) {
+                            break;
+                        }
+                    } else {
+                        newValue = oldValue + 1;
+                        if (count.replace("a", oldValue, newValue)) {
+                            break;
+                        }
+                    }
+                }
+            }
+            endLatch.countDown();
+        };
+        new Thread(task).start();
+        new Thread(task).start();
+
+        try {
+            endLatch.await();
+            System.out.println(count);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 由于ConcurrentMap中不能保存value为null的值，所以需要处理不存在和已存在两种情况，不过可以使用AtomicInteger来替代
+     * <p>
+     * 使用AtomicInteger 原子类对demo2进行优化
+     */
+    @Test
+    public void demo3() {
+        final Map<String, AtomicInteger> count = new ConcurrentHashMap<>();
+        final CountDownLatch endLatch = new CountDownLatch(2);
+        Runnable task = () -> {
+            AtomicInteger oldValue;
+            for (int i = 0; i < 5; i++) {
+                oldValue = count.get("a");
+                if (null == oldValue) {
+                    AtomicInteger zeroValue = new AtomicInteger(0);
+                    oldValue = count.putIfAbsent("a", zeroValue);
+                    if (null == oldValue) {
+                        oldValue = zeroValue;
+                    }
+                }
+                oldValue.incrementAndGet();
+            }
+            endLatch.countDown();
+        };
+        new Thread(task).start();
+        new Thread(task).start();
+
+        try {
+            endLatch.await();
+            System.out.println(count);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /****************** demo end********************/
 }
