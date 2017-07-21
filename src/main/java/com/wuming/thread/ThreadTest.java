382cdcada11c61a89b428747d526ec861d90dc89
wuming <952532573@qq.com>
2017-07-21 14:23:29 +0800

    update demo for seek executorService run over

--- src/main/java/com/wuming/thread/ThreadTest.java
+++ src/main/java/com/wuming/thread/ThreadTest.java
@@ -1,42 +1,70 @@
 package com.wuming.thread;
 
-import java.util.HashMap;
+import org.junit.Test;
+
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by wuming on 2017/6/29.
+ * 在使用多线程的时候有时候我们会使用 java.util.concurrent.Executors的线程池，
+ * 当多个线程异步执行的时候，我们往往不好判断是否线程池中所有的子线程都已经执行完毕，
+ * 但有时候这种判断却很有用，例如我有个方法的功能是往一个文件异步地写入内容，
+ * 我需要在所有的子线程写入完毕后在文件末尾写“---END---”及关闭文件流等，
+ * 这个时候我就需要某个标志位可以告诉我是否线程池中所有的子线程都已经执行完毕，我使用这种方式来判断。
  */
 public class ThreadTest {
 
-    /**
-     * 由于 HashMap 非线程安全，多个线程同时像map中put时，
-     * 假如key相同，会造成map被hold住的情况(具体原因是map的put操作可能会resize map，相同key同时resize时就会出问题了)
-     * 我运行了多次并没有复现这种情况
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        final HashMap map = new HashMap();
-        final Thread t1 = new Thread() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 5000000; i++) {
-                    map.put(new Integer(i), i);
-                }
-                System.out.println("t1 over");
-            }
-        };
 
-        final Thread t2 = new Thread() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 5000000; i++) {
-                    map.put(new Integer(i), i);
+    @Test
+    public void test() throws IOException, InterruptedException {
+        final File stream = new File("c:\\temp\\stonefeng\\stream.txt");
+        final OutputStream os = new FileOutputStream(stream);
+        final OutputStreamWriter writer = new OutputStreamWriter(os);
+        final Semaphore semaphore = new Semaphore(10);
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        final long start = System.currentTimeMillis();
+        for (int i = 0; i < 10000000; i++) {
+            final int num = i;
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        semaphore.acquire();
+                        writer.write(String.valueOf(num) + "\n");
+                        semaphore.release();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-                System.out.println("t2 over");
+            };
+            exec.submit(task);
+        }
+
+        exec.shutdown();
+
+        // 可以使用以下死循环每隔1s判断是否执行完，返回true时，则跳出死循环
+        while (true) {
+            if (exec.isTerminated()) {
+                writer.write("---END---\n");
+                writer.close();
+                System.out.println("所有的子线程都结束了！");
+                break;
             }
-        };
-        t1.start();
-        t2.start();
+            Thread.sleep(1000);
+        }
+
+        // 以上死循环改进，可以直接使用以下这行代码代替
+        exec.awaitTermination(1, TimeUnit.HOURS);
+
+        final long end = System.currentTimeMillis();
+        System.out.println((end - start) / 1000);
     }
 
 }
